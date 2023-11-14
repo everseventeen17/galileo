0ca3dc24d03b7cf4a4a9691eabb1416a25b11846
DaniilDaniil <daniil.eudokimou@gmail.com>
2023-11-14 16:05:15 +0300

    ver 0.0.1

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="./assets/css/index.css">
+</head>
+<body>
+<header class="header">
+    <div class="container">
+        <div class="header__img-wrapper">
+            <img class="header__img-wheel_type_black" src="./assets/img/header/logo-wheel-1.svg" decoding="async"
+                 alt="wheel-black">
+            <img class="header__img-wheel_type_red" src="./assets/img/header/logo-wheel-2.svg" decoding="async"
+                 alt="wheel-red">
+            <img class="header__img-eye" src="./assets/img/header/eye.svg" decoding="async" alt="eye">
+            <img class="header__img" src="./assets/img/header/logo__name.svg" decoding="async" alt="Галилео">
+        </div>
+        <div class="header__city-wrapper">
+            <svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1.00684 1.5L7.00684 7.5L13.0068 1.5" stroke="#E63323" stroke-width="2"/>
+            </svg>
+            <p class="header__city-adress">
+                ул. Добролюбова, 16а, 3 этаж
+                пн-вс с 10:00 до 20:00
+                касса работает до 19:00
+            </p>
+        </div>
+        <div class="header__telephone-wrapper">
+            <a class="header__telephone" href="">+7 (383) 312-05-09</a>
+            <button class="header__btn button">Купить билет</button>
+        </div>
+    </div>
+</header>
+
+<section></section>
+
+<footer></footer>
+<script src="./assets/js/index.js"></script>
+</body>
+</html>
