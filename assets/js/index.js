bbb97c627563f171eeda7f51260d923960890e3f
DaniilDaniil <daniil.eudokimou@gmail.com>
2023-11-15 17:34:40 +0300

    ver 0.0.5

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+    $(".slide-one").owlCarousel({
         items:1,
         loop:true,
         margin:20,
@@ -7,6 +7,21 @@ $(document).ready(function(){
         autoplayTimeout:100000,
         autoplayHoverPause:true
     });
+    $(".slide-two").owlCarousel({
+        items:3,
+        loop:true,
+        dots: false,
+        nav: true,
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        margin:20,
+        autoplay:true,
+        autoplayTimeout:100000,
+        autoplayHoverPause:true
+    });
+    $(".dropdown-head").on("click", function (e){
+        $(this).toggleClass('active')
+        $(this).parent('.dropdown').find('.dropdown-slide').slideToggle();
+    })
 });
 
 
--- index.php
+++ index.php
@@ -34,7 +34,7 @@
     </div>
 </header>
 
-<section class="slider__section owl-carousel content">
+<section class="slider__section owl-carousel slide-one content">
     <div class="slider__item">
         <div class="slider__content">
             <h1 class="">Фантастическое новогоднее
@@ -214,33 +214,296 @@
         <h2 class="affiche__title section__title">Афиша новогодних мероприятий в «Парке чудес Галилео»</h2>
     </div>
     <div class="affiche__content">
-        <div class="container">
+        <div class="container container_large">
             <div class="affiche__items">
                 <div class="affiche__item">
                     <p class="affiche__date">19 декабря, пн</p>
-                    <button class="affiche__time">12:00</button>
-                    <button class="affiche__time affiche__time_type_empty"></button>
-                    <button class="affiche__time">12:00</button>
+                    <div class="affiche__time-wrapper">
+                        <button class="affiche__time">12:00</button>
+                        <button class="affiche__time affiche__time_type_empty"></button>
+                        <button class="affiche__time">12:00</button>
+                    </div>
                 </div>
                 <div class="affiche__item">
-                    <p class="affiche__date">25 декабря, пн</p>
-                    <button class="affiche__time">12:00</button>
-                    <button class="affiche__time affiche__time_type_empty"></button>
-                    <button class="affiche__time">12:00</button>
+                    <p class="affiche__date">19 декабря, пн</p>
+                    <div class="affiche__time-wrapper">
+                        <button class="affiche__time">12:00</button>
+                        <button class="affiche__time affiche__time_type_empty"></button>
+                        <button class="affiche__time">12:00</button>
+                    </div>
                 </div>
                 <div class="affiche__item">
-                    <p class="affiche__date">26 декабря, пн</p>
-                    <button class="affiche__time">12:00</button>
-                    <button class="affiche__time affiche__time_type_empty"></button>
-                    <button class="affiche__time">12:00</button>
+                    <p class="affiche__date">19 декабря, пн</p>
+                    <div class="affiche__time-wrapper">
+                        <button class="affiche__time">12:00</button>
+                        <button class="affiche__time affiche__time_type_empty"></button>
+                        <button class="affiche__time">12:00</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="price__section">
+    <img src="./assets/img/program/patern.png" decoding="async" width="287" height="1117" alt="patern_left"
+         class="program__img_type_left">
+
+    <div class="price__content">
+        <div class="container">
+            <div class="inner-wrapper">
+                <h2 class="price__title section__title">Стоимость билетов</h2>
+                <p class="price__text">Билет приобретается на каждого взрослого и каждого ребёнка. Билет действует на
+                    дату и время мероприятия, на
+                    которое он приобретался.Для каждого ребенка младше 18 лет требуется сопровождение взрослого, с 1
+                    взрослым мжет
+                    пройти не более 3х детей младше 18 лет. Мы не рекомендуем посещать мероприятие детям младше 5 лет,
+                    т.к. квест
+                    будет проходить различных локациях, где ребёнок может испугаться (темный и зеркальный лабиринт)</p>
+                <div class="price__tickets">
+                    <div class="price__ticket">
+                        <div class="price__ticket-top">
+                            <img class="price__ticket-img" src="./assets/img/price/price__ticket.svg" width="50"
+                                 height="57" decoding="async" alt="ticket">
+                            <p class="price__ticket-title">Детский билет</p>
+                        </div>
+                        <div class="price__ticket-mid">
+                            <p class="price__ticket-text">Путешествие по параллельным мирам
+                                во время фантастического квеста.
+                            </p>
+                            <p class="price__ticket-text">
+                                Участие в фееричной бумажной
+                                дискотеке
+                            </p>
+                        </div>
+                        <div class="price__ticket-footer">
+                            <p class="price__ticket-sum">1500 ₽</p>
+                        </div>
+                    </div>
+                    <div class="price__ticket">
+                        <div class="price__ticket-top">
+                            <img class="price__ticket-img" src="./assets/img/price/price__ticket.svg" width="50"
+                                 height="57" decoding="async" alt="ticket">
+                            <p class="price__ticket-title">Детский билет</p>
+                        </div>
+                        <div class="price__ticket-mid">
+                            <p class="price__ticket-text">Путешествие по параллельным мирам
+                                во время фантастического квеста.
+                            </p>
+                            <p class="price__ticket-text">
+                                Участие в фееричной бумажной
+                                дискотеке
+                            </p>
+                        </div>
+                        <div class="price__ticket-footer">
+                            <p class="price__ticket-sum">1500 ₽</p>
+                        </div>
+                    </div>
+                    <div class="price__ticket">
+                        <div class="price__ticket-top">
+                            <img class="price__ticket-img" src="./assets/img/price/price__ticket.svg" width="50"
+                                 height="57" decoding="async" alt="ticket">
+                            <p class="price__ticket-title">Детский билет</p>
+                        </div>
+                        <div class="price__ticket-mid">
+                            <p class="price__ticket-text">Путешествие по параллельным мирам
+                                во время фантастического квеста.
+                            </p>
+                            <p class="price__ticket-text">
+                                Участие в фееричной бумажной
+                                дискотеке
+                            </p>
+                        </div>
+                        <div class="price__ticket-footer">
+                            <p class="price__ticket-sum">1500 ₽</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="price__special">
+                    <div class="dropdown">
+                        <div class="price__special-dropdown-head dropdown-head">
+                            <h4 class="price__special-title">
+                                Специальное предложение для групп
+                            </h4>
+                            <svg width="14" height="9" viewBox="0 0 14 9" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1.00684 1.5L7.00684 7.5L13.0068 1.5" stroke="#FFFF" stroke-width="2"/>
+                            </svg>
+                        </div>
+                        <div class="price__special-slide dropdown-slide">
+                            <p class="price__special-text">Текст про специальное предложение для группового посещения.
+                                Текст
+                                про специальное предложение для
+                                группового посещения. Текст про специальное предложение для группового посещения. Текст
+                                про
+                                специальное
+                                предложение для группового посещения. Текст про специальное предложение для группового
+                                посещения.
+                                Текст про специальное предложение для группового посещения. Текст про специальное
+                                предложение для
+                                группового посещения. Текст про специальное предложение для группового посещения. Текст
+                                про
+                                специальное предложение для группового посещения
+                            </p>
+                            <button class="price__special-btn button button-yellow"><b>Забронировать для группы</b>
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="price__inner-wrapper inner-wrapper">
+                <img class="price__img-attention" src="./assets/img/price/attention.svg" alt="attention" width=""
+                     height="" decoding="async">
+                <p class="price__text">
+                    <b>
+                        Перед посещением необходимо ознакомиться с правилами индивидуального
+                        посещения!
+                    </b>
+                </p>
+                <button class="button">Ознакомиться</button>
+            </div>
+            <div class="price__buttons">
+                <button class="button">Купить билет</button>
+                <button class="button button-yellow"><b>Забронировать</b></button>
+
+            </div>
+        </div>
+    </div>
+
+    <img src="./assets/img/program/patern_right.png" decoding="async" width="287" height="1117" alt="patern_right"
+         class="program__img_type_right">
+
+</section>
+
+<section class="tree__section">
+    <div class="section__title-wrapper section__title-wrapper_type_green">
+        <h2 class="tree__title section__title">Фантастичекая ёлка в «Парке чудес Галилео» это:</h2>
+    </div>
+    <div class="tree__content">
+        <div class="container">
+            <div class="tree__items owl-carousel slide-two">
+                <div class="slider__item">
+                    <img class="slider__img" src="./assets/img/tree/tree__item-1.png" width="278" height="211"
+                         decoding="async" alt="item">
+                    <p class="slider__text">Искренний
+                        восторг1</p>
+                </div>
+                <div class="slider__item">
+                    <img class="slider__img" src="./assets/img/tree/tree__item-1.png" width="278" height="211"
+                         decoding="async" alt="item">
+                    <p class="slider__text">Искренний
+                        восторг2</p>
+                </div>
+                <div class="slider__item">
+                    <img class="slider__img" src="./assets/img/tree/tree__item-1.png" width="278" height="211"
+                         decoding="async" alt="item">
+                    <p class="slider__text">Искренний
+                        восторг3</p>
+                </div>
+                <div class="slider__item">
+                    <img class="slider__img" src="./assets/img/tree/tree__item-1.png" width="278" height="211"
+                         decoding="async" alt="item">
+                    <p class="slider__text">Искренний
+                        восторг4</p>
+                </div>
+                <div class="slider__item">
+                    <img class="slider__img" src="./assets/img/tree/tree__item-1.png" width="278" height="211"
+                         decoding="async" alt="item">
+                    <p class="slider__text">Искренний
+                        восторг5</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="faq__section">
+    <div class="container">
+        <h2 class="faq__section-title section__title">Вопросы — ответы</h2>
+        <div class="faq__content">
+            <div class="faq__dropdowns">
+                <div class="faq__dropdown dropdown">
+                    <div class="faq__dropdown-head dropdown-head">
+                        <h4 class="faq__title">
+                            Специальное предложение для групп
+                        </h4>
+                        <svg width="14" height="9" viewBox="0 0 14 9" fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1.00684 1.5L7.00684 7.5L13.0068 1.5" stroke="#E63323" stroke-width="2"/>
+                        </svg>
+                    </div>
+                    <div class="faq__slide dropdown-slide" style="display: none">
+                        <p class="faq__text">
+                            "Парк чудес Галилео" в Новосибирске - это удивительный интерактивный музей, в котором
+                            представлены уникальные экспонаты и конструкции, невероятные иллюзии и огромный зеркальный
+                            лабиринт. Работа всех экспонатов основана на законах физики, математики, объяснении как
+                            устроен окружающий мир и демонстрации особенностей человеческого организма. Трогайте,
+                            крутите, нажимайте, соревнуйтесь в ловкости и смекалке. Почувствуйте себя первооткрывателем!
+                            В "Парке чудес Галилео" будет интересно гостям разного возраста от 6 до 99 лет. Каждый для
+                            себя найдет что-то особенное. Также вас ждут Лаборатория чудес с невероятными химическими
+                            опытами, магазин развивающих игр и кафетерий с вкусными десертами и напитками.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <img class="faq__img" src="./assets/img/faq/faq_img.png" alt="faq" width="304" height="380"
+                 decoding="async">
+        </div>
+
+    </div>
+</section>
+
+<footer>
+    <div class="container">
+        <div class="footer__top">
+            <div class="footer__content">
+                <h2 class="footer__title">Приходите!
+                    Мы вас ждём)</h2>
+                <p class="footer__text">
+                    Новосибирск, ул. Добролюбова, 16а, 3 этаж пн-вс с 10:00 до 20:00 Касса работает до 19:00
+                </p>
+                <a class="footer__phone" href="#">+7 (383) 312-05-09</a>
+                <button class="button">Заказать звонок</button>
+            </div>
+            <div class="footer__map-wrapper">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2287.948933019668!2d82.9532244!3d55.0090602!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x42dfe611977b9e79%3A0x10bd55f7882d2d1f!2z0YPQuy4g0JTQvtCx0YDQvtC70Y7QsdC-0LLQsCwgMTbQkCwg0J3QvtCy0L7RgdC40LHQuNGA0YHQuiwg0J3QvtCy0L7RgdC40LHQuNGA0YHQutCw0Y8g0L7QsdC7LiwgNjMwMDA5!5e0!3m2!1sru!2sru!4v1700058124966!5m2!1sru!2sru"
+                        width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </div>
+    <div class="footer__bottom">
+        <div class="container">
+            <nav class="footer__navigation">
+                <ul class="footer__navigation-list">
+                    <li class="footer__navigation-item"><a href="#">Главная</a></li>
+                    <li class="footer__navigation-item"><a href="#">Главная</a></li>
+                    <li class="footer__navigation-item"><a href="#">Главная</a></li>
+                    <li class="footer__navigation-item"><a href="#">Главная</a></li>
+                    <li class="footer__navigation-item"><a href="#">Главная</a></li>
+                </ul>
+            </nav>
+            <div class="footer__stuff">
+                <ul class="footer__rules-list">
+                    <li class="footer__rules-item">Правила</li>
+                    <li class="footer__rules-item">Правила</li>
+                    <li class="footer__rules-item">Правила</li>
+                </ul>
+                <div class="footer__socials">
+
                 </div>
             </div>
         </div>
     </div>
 
-</section>
-
-<footer></footer>
+    <p class="footer__copyright">
+        copyright Galileo
+    </p>
+</footer>
 <script src="./assets/js/libs/jQuery_v3.7.1/jQuery_v3.7.1.js"></script>
 <script src="./assets/js/libs/owl-carousel/owl.carousel.min.js"></script>
 <script src="./assets/js/index.js"></script>
