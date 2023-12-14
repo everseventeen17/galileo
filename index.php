0509f49da56ddac4f6ef2187b5bbdc8afe316d34
DaniilDaniil <daniil.eudokimou@gmail.com>
2023-12-14 11:59:26 +0300

    add mobile to some section

--- index.php
+++ index.php
@@ -12,31 +12,36 @@
     <div class="container">
         <div class="header__right-wrapper">
             <div class="header__img-wrapper">
-                <img class="header__img-wheel_type_black" src="./assets/img/header/logo-wheel-1.svg" decoding="async"
+                <img class="header__img-wheel_type_black desktop__logo" src="./assets/img/header/logo-wheel-1.svg" decoding="async"
                      alt="wheel-black">
-                <img class="header__img-wheel_type_red" src="./assets/img/header/logo-wheel-2.svg" decoding="async"
+                <img class="header__img-wheel_type_red desktop__logo" src="./assets/img/header/logo-wheel-2.svg" decoding="async"
                      alt="wheel-red">
-                <img class="header__img-eye" src="./assets/img/header/eye.svg" decoding="async" alt="eye">
-                <img class="header__img" src="./assets/img/header/logo__name.svg" decoding="async" alt="Галилео">
+                <img class="header__img-eye desktop__logo" src="./assets/img/header/eye.svg" decoding="async" alt="eye">
+                <img class="header__img desktop__logo" src="./assets/img/header/logo__name.svg" decoding="async" alt="Галилео">
+                <img class="header__img mobile__logo" src="./assets/img/header/logo-mobile.svg" decoding="async" alt="Галилео">
             </div>
-            <div class="header__city-wrapper">
-                <svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1.00684 1.5L7.00684 7.5L13.0068 1.5" stroke="#E63323" stroke-width="2"/>
-                </svg>
-                <p class="header__city-adress">
-                    ул. Добролюбова, 16а, 3 этаж
-                    пн-вс с 10:00 до 20:00
-                    касса работает до 19:00
-                </p>
+            <div class="dropdown">
+                <div class="header__city-wrapper dropdown-head">
+                    <svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1.00684 1.5L7.00684 7.5L13.0068 1.5" stroke="#E63323" stroke-width="2"/>
+                    </svg>
+                    <p class="header__city-adress">
+                        ул. Добролюбова, 16а, 3 этаж
+                        пн-вс с 10:00 до 20:00
+                        касса работает до 19:00
+                    </p>
+                    <div class="dropdown-slide">
+                        <p class="header__dropdown__text">
+                            "Парк чудес Галилео" в Новосибирске - это удивительный интерактивный музей.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
 
         <div class="header__wrapper-left">
-            <div class="header__telephone-wrapper">
-                <a class="header__telephone" href="#">+7 (383) 312-05-09</a>
-                <button class="header__btn button">Купить билет</button>
-            </div>
             <nav class="mobile-menu">
+                 <b><p>МЕНЮ</p></b>
                 <a href="#" class="hamburger-wrapper">
                     <div class="hamburger-menu"></div>
                 </a>
@@ -51,65 +56,71 @@
                     </ul>
                 </div>
             </nav>
+            <div class="header__telephone-wrapper">
+                <a class="header__telephone" href="#">+7 (383) 312-05-09</a>
+                <button class="header__btn button">Купить билет</button>
+            </div>
         </div>
     </div>
 </header>
 
-<!--<section class="slider__section owl-carousel slide-one content">-->
-<!--    <div class="slider__item">-->
-<!--        <div class="slider__content">-->
-<!--            <h1 class="">Фантастическое новогоднее-->
-<!--                приключение для детей в «Парке чудес Галилео»!</h1>-->
-<!--            <button class="slider__btn button">Купить билет</button>-->
-<!--        </div>-->
-<!--        <div class="slider__img-wrapper">-->
-<!--            <img src="./assets/img/slider/slider_item.png" alt="slider_item" decoding="async" height="395" width="659">-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div class="slider__item">-->
-<!--        <div class="slider__content">-->
-<!--            <h1 class="">Фантастическое новогоднее-->
-<!--                приключение для детей в «Парке чудес Галилео»!</h1>-->
-<!--            <button class="slider__btn button">Купить билет</button>-->
-<!--        </div>-->
-<!--        <div class="slider__img-wrapper">-->
-<!--            <img src="./assets/img/slider/slider_item.png" alt="slider_item" decoding="async" height="395" width="659">-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</section>-->
+<section class="slider__section owl-carousel slide-one content">
+    <div class="slider__item">
+        <div class="slider__content">
+            <h1 class="slider__title">Фантастическое новогоднее
+                приключение для детей в «Парке чудес Галилео»!</h1>
+            <button class="slider__btn button">Купить билет</button>
+        </div>
+        <div class="slider__img-wrapper">
+            <img src="./assets/img/slider/slider_item.png" alt="slider_item" decoding="async" height="395" width="659">
+        </div>
+    </div>
+    <div class="slider__item">
+        <div class="slider__content">
+            <h1 class="slider__title">Фантастическое новогоднее
+                приключение для детей в «Парке чудес Галилео»!</h1>
+            <button class="slider__btn button">Купить билет</button>
+        </div>
+        <div class="slider__img-wrapper">
+            <img src="./assets/img/slider/slider_item.png" alt="slider_item" decoding="async" height="395" width="659">
+        </div>
+    </div>
+</section>
+
+<section class="navigation__section">
+    <div class="container">
+        <nav class="navigation">
+            <ul class="navigation__list">
+                <li class="navigation__list-item"><a href="#">О мероприятии</a></li>
+                <li class="navigation__list-item"><a href="#">Для кого?</a></li>
+                <li class="navigation__list-item"><a href="#">Программа праздника</a></li>
+                <li class="navigation__list-item"><a href="#">Афиша</a></li>
+                <li class="navigation__list-item"><a href="#">Стоимость</a></li>
+                <li class="navigation__list-item"><a href="#">Ответы на вопросы</a></li>
+            </ul>
+        </nav>
+    </div>
+</section>
 <!---->
-<!--<section class="navigation__section">-->
-<!--    <div class="container">-->
-<!--        <nav class="navigation">-->
-<!--            <ul class="navigation__list">-->
-<!--                <li class="navigation__list-item"><a href="#">О мероприятии</a></li>-->
-<!--                <li class="navigation__list-item"><a href="#">Для кого?</a></li>-->
-<!--                <li class="navigation__list-item"><a href="#">Программа праздника</a></li>-->
-<!--                <li class="navigation__list-item"><a href="#">Афиша</a></li>-->
-<!--                <li class="navigation__list-item"><a href="#">Стоимость</a></li>-->
-<!--                <li class="navigation__list-item"><a href="#">Ответы на вопросы</a></li>-->
-<!--            </ul>-->
-<!--        </nav>-->
-<!--    </div>-->
-<!--</section>-->
-<!---->
-<!--<section class="about__section">-->
-<!--    <div class="container">-->
-<!--        <div class="inner-wrapper">-->
-<!--            <div class="about__top">-->
-<!--                <p class="about__text">На новогоднем празднике в «Парке чудес Галилео» нет хоровода вокруг ёлки, зато-->
-<!--                    есть: <b>фантастический квест</b>, во время которого гости отправятся в путешствие по параллельным-->
-<!--                    мирам. <b>Спецагенты</b>, которые-->
-<!--                    создают таинственную атмосферу и сопровождают вас в-->
-<!--                    увлекательном приключении. <b>Оригинальные подарки</b> от Деда Мороза,-->
-<!--                    которые приведут в восторг каждого гостя и будут напоминать о-->
-<!--                    классном празднике ещё долгое время. <b>Бумажная дискотека</b>, которая-->
-<!--                    станет фееричным завершением мероприятия и зарядит всех гостей-->
-<!--                    новогодним настроением! Мероприятие длится 3 часа и подходит детям-->
-<!--                    от 6 до 12 лет.</p>-->
-<!--                <img src="./assets/img/about/about__img.png" class="about__img" alt="presents" decoding="async"-->
-<!--                     width="294" height="330">-->
-<!--            </div>-->
+<section class="about__section">
+    <div class="container">
+        <div class="inner-wrapper">
+            <div class="about__top">
+                <p class="about__text">На новогоднем празднике в «Парке чудес Галилео» нет хоровода вокруг ёлки, зато
+                    есть: <b>фантастический квест</b>, во время которого гости отправятся в путешствие по параллельным
+                    мирам. <b>Спецагенты</b>, которые
+                    создают таинственную атмосферу и сопровождают вас в
+                    увлекательном приключении. <b>Оригинальные подарки</b> от Деда Мороза,
+                    которые приведут в восторг каждого гостя и будут напоминать о
+                    классном празднике ещё долгое время. <b>Бумажная дискотека</b>, которая
+                    станет фееричным завершением мероприятия и зарядит всех гостей
+                    новогодним настроением! Мероприятие длится 3 часа и подходит детям
+                    от 6 до 12 лет.</p>
+                <img src="./assets/img/about/about__img.png" class="about__img desktop__img" alt="presents" decoding="async"
+                     width="295" height="330">
+                <img src="./assets/img/about/about-mobile-img.png" class="about__img mobile__img" alt="presents" decoding="async"
+                     width="71" height="72">
+            </div>
 <!--            <div class="about__buttons">-->
 <!--                <button class="about__btn button">Афиша</button>-->
 <!--                <button class="about__btn button">Купить билет</button>-->
@@ -117,10 +128,10 @@
 <!--            <div class="about__video">-->
 <!--                <div class="about__video-play"></div>-->
 <!--            </div>-->
-<!---->
-<!--        </div>-->
-<!--    </div>-->
-<!--</section>-->
+
+        </div>
+    </div>
+</section>
 <!---->
 <!--<section class="program__section">-->
 <!--    <div class="section__title-wrapper">-->
